7a8b8f05ae2484cd411f8c3acb02e75abd3a8544
Bastian Stegmann <bastian.stegmann@vougee.gmbh>
2023-07-25 16:49:16 +0200

    Updated provisioning script

--- provisioning.sh
+++ provisioning.sh
@@ -1,12 +1,29 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
-export DEBIAN_FRONTEND=noninteractive
+set -euo pipefail
 
-apt-get update
-apt-get install --yes git curl
+# Detect OS and distribution
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+else
+  echo "Provisioning: Could not detect OS"
+  exit 1
+fi
 
-curl -s "https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh" | bash
-apt-get install --yes git-lfs
+if [[ "$ID_LIKE" == "debian" ]]; then
+  export DEBIAN_FRONTEND=noninteractive
+  arch=$(dpkg --print-architecture)
 
-curl -L --output /usr/local/bin/gitlab-runner "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"
-chmod +x /usr/local/bin/gitlab-runner
+  apt-get update
+  apt-get install --yes git curl
+
+  curl -s "https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh" | bash
+  apt-get install --yes git-lfs
+
+  curl -L --output /usr/local/bin/gitlab-runner "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-$arch"
+  chmod +x /usr/local/bin/gitlab-runner
+else
+  echo "This distribution is not supported!"
+  echo "ID: ${ID}"
+  echo "ID_LIKE: ${ID_LIKE}"
+fi
