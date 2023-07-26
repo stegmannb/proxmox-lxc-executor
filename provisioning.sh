59e9d7278aa12736924376009c047e332a584535
Bastian Stegmann <bastian.stegmann@vougee.gmbh>
2023-07-26 20:34:03 +0200

    Updated provisioning.sh script

--- provisioning.sh
+++ provisioning.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # Detect OS and distribution
 if [[ -f "/etc/os-release" ]]; then
-    . "/etc/os-release"
+  . "/etc/os-release"
 else
   echo "Provisioning: Could not detect OS"
   exit 1
@@ -24,7 +24,10 @@ if [[ "$ID" == "debian" || "${ID_LIKE:-empty}" == "debian" ]]; then
   chmod +x /usr/local/bin/gitlab-runner
 
 elif [[ "$ID" == "arch" || "${ID_LIKE:-empty}" == "arch" ]]; then
-  pacman -Sy --noconfirm --needed curl base-devel git git-lfs
+  pacman-key --init
+  pacman-key --populate archlinux
+  pacman -Syuu --noconfirm --needed base-devel curl git git-lfs
+#  pacman -Sy --noconfirm --needed
 
   uname_machine=$(uname --machine)
   if [[ "x86_64" == "$uname_machine" ]]; then
